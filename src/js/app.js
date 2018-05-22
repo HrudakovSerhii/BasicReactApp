c2b00353b680dd73346c7ed472754f54e2222425
Serhiy Grudakov <Serhiy@MacBook-Pro-Serhiy.local>
2018-05-22 19:03:45 +0200

    Finalised base app settings

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Serhiy on 14.05.18.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	render() {
+		return <h1>Hello, world</h1>;
+	}
+}
+
+export default App;
+
